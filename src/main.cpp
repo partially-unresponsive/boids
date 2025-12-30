c9b3d24330cb5fc6637cafde46a7dac611526e87
partially-unresponsive <unsecuredmail@proton.me>
2025-12-30 23:05:00 +0200

    Boids: Added player avoidance functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "RealVector.hpp"
 #include "raylib.h"
 #include "config.hpp"
 #include "Boid.hpp"
@@ -5,7 +6,7 @@
 #include <time.h>
 #include <vector>
 using std::vector;
-// #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
 // TODO: Figure out how to avoid needing to use the raylib template repo every time, see https://youtu.be/8o-d9WUsdhs?t=323 and -lraylib, potentially also the .vscode folder
 
@@ -20,7 +21,7 @@ int main ()
 
 	// TODO: Adjut factors here!
 	double turn_factor = 0.2, avoidance_factor = 0.1, matching_factor = 0.05, centering_factor = 0.0005;
-	float turn_padding = 100, min_speed = 3, max_speed = 6;
+	float turn_padding = 100, min_speed = 6, max_speed = 12;
 	float danger_zone = 15, sight_zone = 40, size = 10;
 
 	srand(time(NULL));
@@ -33,16 +34,31 @@ int main ()
 	}
 	Boid boid(100, 100, 1, 1, 15, 40, 10);
 
-	// game loop
+
+	RealVector player(0, 0);
+	float player_radius = 40.0f;
+
+	SearchAndSetResourceDir("resources");
+
+	Texture wabbit = LoadTexture("wabbit_alpha.png");
+
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
 	{
 		BeginDrawing();
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
+		player.x = GetMouseX();
+		player.y = GetMouseY();
+		// DrawCircle(player.x, player.y, player_radius, RED);
+		DrawTexture(wabbit, player.x-20, player.y-20, RED);
 
 		for (int i = 0; i < boids.size(); i++){
 			alter_boid_path(boids, i, avoidance_factor, matching_factor, centering_factor);
+
+			RealVector diff = boids[i].position.sub(player);
+			if (diff.getMag() < boids[i].sight_zone + player_radius) boids[i].velocity = boids[i].velocity.add(diff);
+
 			limit_speed(boids[i], min_speed, max_speed);
 			bound_boid(boids[i], turn_factor, turn_padding, SCREEN_WIDTH, SCREEN_HEIGHT);
 		}
